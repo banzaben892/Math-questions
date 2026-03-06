f437b5324da7526ddef1766877c76e051cd8f7d0
banzaben892 <banzaben892@gmail.com>
2026-03-06 09:40:01 +0200

    Create GameActivity.kt

---
+++ app/src/main/activities/GameActivity.kt
@@ -0,0 +1,25 @@
+package com.example.flappycoin.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flappycoin.ui.GameView
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var gameView: GameView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gameView = GameView(this)
+        setContentView(gameView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gameView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameView.resume()
+    }
+}
